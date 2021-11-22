4972a92155fe79ad0749381dc2d032ad6b8b9c26
Val Aleksenko <v.aleksenko@gmail.com>
2021-11-22 10:27:34 -0500

    d2p1

---
+++ lib/day02/part1.ex
@@ -0,0 +1,24 @@
+defmodule AoC2020.Day02.Part1 do
+  @behaviour AoC2020.Day
+
+  @impl AoC2020.Day
+  def run(data) do
+    data |> Enum.count(&valid_password?/1)
+  end
+
+  defp valid_password?(entry) do
+    record = parse(entry)
+    count = record.password |> String.graphemes |> Enum.count(&(record.char == &1))
+
+    count in record.rule
+  end
+
+  defp parse(entry) do
+    [ rule_field, char, password ] = String.split(entry, ~r/[ :]+/)
+    %{ rule: to_rule(rule_field), char: char, password: password }
+  end
+
+  defp to_rule(field) do
+    field |> String.split("-") |> (fn [ first, last ] -> String.to_integer(first) .. String.to_integer(last) end).()
+  end
+end
---
+++ test/day02/part1_test.exs
@@ -0,0 +1,13 @@
+defmodule AoC2020.Day02.Part1Test do
+  use ExUnit.Case
+  doctest AoC2020.Day02.Part1
+  import AoC2020.Day02.Part1
+
+  test "checks for sample input" do
+    assert 2 == run [
+      "1-3 a: abcde",
+      "1-3 b: cdefg",
+      "2-9 c: ccccccccc",
+    ]
+  end
+end
